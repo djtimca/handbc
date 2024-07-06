7c285e07ba29063b237c5f2f85f9525035c3b559
Tim Empringham <administrator@Tims-MacBook-Air.local>
2024-07-06 18:05:11 -0400

    Change from async_forward_entry_setup to async_forward_entry_setups per warning in 2024.7

--- custom_components/ndbcrealtime/__init__.py
+++ custom_components/ndbcrealtime/__init__.py
@@ -63,10 +63,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         COORDINATOR: coordinator,
     }
 
-    for component in PLATFORMS:
-        _LOGGER.info("Setting up platform: %s", component)
-        await hass.config_entries.async_forward_entry_setup(entry, component)
-
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    
     return True
 
 
